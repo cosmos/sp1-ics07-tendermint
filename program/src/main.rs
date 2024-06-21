a1d089750bfb396c58f082660a6c9334b968c9c5
srdtrk <srdtrk@hotmail.com>
2024-06-21 14:04:07 +0400

    feat: program half done

--- program/src/main.rs
+++ program/src/main.rs
@@ -11,8 +11,15 @@
 #![no_main]
 sp1_zkvm::entrypoint!(main);
 
-use ibc_client_tendermint::types::{ConsensusState, Header};
+use std::{str::FromStr, time::Duration};
+
+use ibc_client_tendermint::{
+    client_state::verify_header,
+    types::{ConsensusState, Header},
+};
+use ibc_core_host::types::identifiers::{ChainId, ClientId};
 use sp1_ics07_tendermint_update_client_program::types;
+use tendermint_light_client_verifier::{options::Options, ProdVerifier};
 
 /// The main function of the program.
 ///
@@ -20,11 +27,32 @@ use sp1_ics07_tendermint_update_client_program::types;
 /// Panics if the verification fails.
 pub fn main() {
     // input 1: the trusted consensus state
-    let _trusted_consensus_state = sp1_zkvm::io::read::<ConsensusState>();
+    let trusted_consensus_state = sp1_zkvm::io::read::<ConsensusState>();
     // input 2: the proposed header
-    let _proposed_header = sp1_zkvm::io::read::<Header>();
+    let proposed_header = sp1_zkvm::io::read::<Header>();
     // input 3: environment
-    let _env = sp1_zkvm::io::read::<types::Env>();
+    let env = sp1_zkvm::io::read::<types::Env>();
+
+    let client_id = ClientId::from_str(&env.client_id).unwrap();
+    let chain_id = ChainId::from_str(&env.chain_id).unwrap();
+    let options = Options {
+        trust_threshold: env.trust_threshold,
+        // TODO: check if this is nanos
+        // 2 week trusting period.
+        trusting_period: Duration::from_nanos(env.trusting_period),
+        clock_drift: Duration::default(),
+    };
+    let ctx = types::ClientValidationCtx::new(env, trusted_consensus_state);
+
+    verify_header::<_, sha2::Sha256>(
+        &ctx,
+        &proposed_header,
+        &client_id,
+        &chain_id,
+        &options,
+        &ProdVerifier::default(),
+    )
+    .unwrap();
 
     todo!()
 }
--- program/src/types/mod.rs
+++ program/src/types/mod.rs
@@ -1,11 +1,18 @@
 //! Containes types used in the program.
 
+use ibc_client_tendermint::{
+    client_state::ClientState as ClientStateWrapper,
+    consensus_state::ConsensusState as ConsensusStateWrapper, types::ConsensusState,
+};
+use ibc_core_client::context::{ClientValidationContext, ExtClientValidationContext};
+use ibc_core_handler_types::error::ContextError;
+use ibc_primitives::Timestamp;
 use tendermint_light_client_verifier::types::TrustThreshold;
 
 /// The environment passed to the program.
 #[derive(Debug, serde::Deserialize, serde::Serialize)]
 pub struct Env {
-    /// The chain ID of the host chain.
+    /// The chain ID of the chain that the client is tracking.
     pub chain_id: String,
     /// The client ID of the client that is being updated.
     pub client_id: String,
@@ -16,4 +23,89 @@ pub struct Env {
     pub trusting_period: u64,
     /// The schema representation is the timestamp in nanoseconds
     pub now: u64,
+}
+
+/// The client validation context.
+pub struct ClientValidationCtx {
+    env: Env,
+    trusted_consensus_state: ConsensusState,
+}
+
+impl ClientValidationCtx {
+    /// Create a new instance of the client validation context.
+    #[must_use]
+    pub const fn new(env: Env, trusted_consensus_state: ConsensusState) -> Self {
+        Self {
+            env,
+            trusted_consensus_state,
+        }
+    }
+}
+
+impl ClientValidationContext for ClientValidationCtx {
+    type ClientStateRef = ClientStateWrapper;
+    type ConsensusStateRef = ConsensusStateWrapper;
+
+    fn client_state(
+        &self,
+        _client_id: &ibc_core_host::types::identifiers::ClientId,
+    ) -> Result<Self::ClientStateRef, ContextError> {
+        // not needed by the `verify_header` function
+        unimplemented!()
+    }
+
+    fn consensus_state(
+        &self,
+        _client_cons_state_path: &ibc_core_host::types::path::ClientConsensusStatePath,
+    ) -> Result<Self::ConsensusStateRef, ContextError> {
+        // This is the trusted consensus state, whether or not it corresponds to the
+        // consensus state path will be checked in solidity.
+        Ok(self.trusted_consensus_state.clone().into())
+    }
+
+    fn client_update_meta(
+        &self,
+        _client_id: &ibc_core_host::types::identifiers::ClientId,
+        _height: &ibc_core_client::types::Height,
+    ) -> Result<(Timestamp, ibc_core_client::types::Height), ContextError> {
+        // not needed by the `verify_header` function
+        unimplemented!()
+    }
+}
+
+impl ExtClientValidationContext for ClientValidationCtx {
+    fn host_timestamp(&self) -> Result<Timestamp, ContextError> {
+        Ok(Timestamp::from_nanoseconds(self.env.now).unwrap())
+    }
+
+    fn host_height(&self) -> Result<ibc_core_client::types::Height, ContextError> {
+        // not needed by the `verify_header` function
+        unimplemented!()
+    }
+
+    fn consensus_state_heights(
+        &self,
+        _client_id: &ibc_core_host::types::identifiers::ClientId,
+    ) -> Result<Vec<ibc_core_client::types::Height>, ContextError> {
+        // not needed by the `verify_header` function
+        unimplemented!()
+    }
+
+    fn next_consensus_state(
+        &self,
+        _client_id: &ibc_core_host::types::identifiers::ClientId,
+        _height: &ibc_core_client::types::Height,
+    ) -> Result<Option<Self::ConsensusStateRef>, ContextError> {
+        // not needed by the `verify_header` function
+        unimplemented!()
+    }
+
+    fn prev_consensus_state(
+        &self,
+        _client_id: &ibc_core_host::types::identifiers::ClientId,
+        _height: &ibc_core_client::types::Height,
+    ) -> Result<Option<Self::ConsensusStateRef>, ContextError> {
+        // not needed by the `verify_header` function
+        unimplemented!()
+    }
 }

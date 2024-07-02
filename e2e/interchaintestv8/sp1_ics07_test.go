464059c790ba3aa55a12011fe2648bebd4a9229f
srdtrk <srdtrk@hotmail.com>
2024-07-02 16:13:20 +0800

    e2e: all working with genesis

--- e2e/interchaintestv8/sp1_ics07_test.go
+++ e2e/interchaintestv8/sp1_ics07_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"os"
+	"os/exec"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -9,6 +11,7 @@ import (
 	"github.com/strangelove-ventures/interchaintest/v8/chain/ethereum"
 
 	"github.com/srdtrk/sp1-ics07-tendermint/e2e/v8/e2esuite"
+	"github.com/srdtrk/sp1-ics07-tendermint/e2e/v8/testvalues"
 )
 
 // SP1ICS07TendermintTestSuite is a suite of tests that wraps the TestSuite
@@ -36,11 +39,20 @@ func (s *SP1ICS07TendermintTestSuite) TestBasic() {
 
 	s.SetupSuite(ctx)
 
-	eth, _ := s.ChainA, s.ChainB
+	eth, simd := s.ChainA, s.ChainB
 
 	s.Require().True(s.Run("Deploy contract", func() {
+		s.Require().NoError(os.Chdir("../.."))
+
+		os.Setenv(testvalues.EnvKeyEthRPC, eth.GetHostRPCAddress())
+		os.Setenv(testvalues.EnvKeyTendermintRPC, simd.GetHostRPCAddress())
+		os.Setenv(testvalues.EnvKeySp1Prover, "network")
+
+		s.Require().NoError(exec.Command("target/release/genesis").Run())
+
+		// TODO: regenerate genesis.json before deploying the contracts
 		stdout, _, err := eth.ForgeScript(ctx, s.UserA.KeyName(), ethereum.ForgeScriptOpts{
-			ContractRootDir:  "../../contracts",
+			ContractRootDir:  "contracts",
 			SolidityContract: "script/SP1ICS07Tendermint.s.sol",
 			RawOptions:       []string{"--legacy", "--json"},
 		})
@@ -49,5 +61,8 @@ func (s *SP1ICS07TendermintTestSuite) TestBasic() {
 		s.contractAddress = s.GetEthAddressFromStdout(string(stdout))
 		s.Require().NotEmpty(s.contractAddress)
 		s.Require().Len(s.contractAddress, 42)
+		s.T().Logf("contract address: %s", s.contractAddress)
+
+		os.Setenv(testvalues.EnvKeyContractAddress, s.contractAddress)
 	}))
 }

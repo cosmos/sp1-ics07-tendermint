511ae23e6659b1040b8b74e6b2fe015a9b137bc9
srdtrk <srdtrk@hotmail.com>
2024-07-12 15:10:40 +0800

    imp: defined basic interface

--- contracts/src/ISP1ICS07Tendermint.sol
+++ contracts/src/ISP1ICS07Tendermint.sol
@@ -1,9 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import {ICS07Tendermint} from "./ics07-tendermint/ICS07Tendermint.sol";
+
 /// @title SP1 ICS07 Tendermint Light Client Interface
 /// @author srdtrk
 /// @notice This interface is used to interact with the SP1 ICS07 Tendermint Light Client
 interface ISP1ICS07Tendermint {
+    /// @notice Get the client state
+    function getClientState()
+        external
+        view
+        returns (ICS07Tendermint.ClientState memory);
 
+    /// @notice Get the consensus state keccak256 hash at the given height
+    function getConsensusStateHash(uint32) external view returns (bytes32);
 }
--- contracts/src/SP1ICS07Tendermint.sol
+++ contracts/src/SP1ICS07Tendermint.sol
@@ -6,13 +6,14 @@ import {UpdateClientProgram} from "./ics07-tendermint/UpdateClientProgram.sol";
 import {MembershipProgram} from "./ics07-tendermint/MembershipProgram.sol";
 import {UpdateClientAndMembershipProgram} from "./ics07-tendermint/UcAndMembershipProgram.sol";
 import {ISP1Verifier} from "@sp1-contracts/ISP1Verifier.sol";
+import {ISP1ICS07Tendermint} from "./ISP1ICS07Tendermint.sol";
 import "forge-std/console.sol";
 
 /// @title SP1 ICS07 Tendermint Light Client
 /// @author srdtrk
 /// @notice This contract implements an ICS07 IBC tendermint light client using SP1.
 /// @custom:poc This is a proof of concept implementation.
-contract SP1ICS07Tendermint {
+contract SP1ICS07Tendermint is ISP1ICS07Tendermint {
     /// @notice The verification key for the update client program.
     bytes32 public immutable updateClientProgramVkey;
     /// @notice The verification key for the verify (non)membership program.
@@ -66,10 +67,10 @@ contract SP1ICS07Tendermint {
         return clientState;
     }
 
-    /// @notice Returns the consensus state at the given revision height.
+    /// @notice Returns the consensus state keccak256 hash at the given revision height.
     /// @param revisionHeight The revision height.
     /// @return The consensus state at the given revision height.
-    function getConsensusState(
+    function getConsensusStateHash(
         uint32 revisionHeight
     ) public view returns (bytes32) {
         return consensusStateHashes[revisionHeight];

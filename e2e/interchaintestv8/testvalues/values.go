1ffbaa076ec8d890e96530a5248efeb351a82c95
srdtrk <srdtrk@hotmail.com>
2024-07-17 12:30:49 +0800

    test: improved e2e

--- e2e/interchaintestv8/testvalues/values.go
+++ e2e/interchaintestv8/testvalues/values.go
@@ -5,6 +5,8 @@ import (
 
 	"cosmossdk.io/math"
 
+	ibctm "github.com/cosmos/ibc-go/v8/modules/light-clients/07-tendermint"
+
 	"github.com/strangelove-ventures/interchaintest/v8/chain/ethereum"
 )
 
@@ -36,4 +38,7 @@ var (
 
 	// StartingEthBalance is the amount of ETH to give to each user at the start of the test.
 	StartingEthBalance = math.NewInt(2 * ethereum.ETHER)
+
+	// DefaultTrustLevel is the trust level used by the SP1ICS07Tendermint contract.
+	DefaultTrustLevel = ibctm.Fraction{Numerator: 2, Denominator: 3}.ToTendermint()
 )

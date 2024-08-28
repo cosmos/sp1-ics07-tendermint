bf39613282d1c73a5ef0c6af1b74a8073e43ba61
Gjermund Garaba <gjermund@garaba.net>
2024-08-28 15:45:08 +0200

    code cleanup

--- e2e/interchaintestv8/sp1_ics07_test.go
+++ e2e/interchaintestv8/sp1_ics07_test.go
@@ -3,9 +3,7 @@ package main
 import (
 	"context"
 	"crypto/ecdsa"
-	"encoding/base64"
 	"encoding/hex"
-	"encoding/json"
 	"os"
 	"strconv"
 	"testing"
@@ -17,13 +15,6 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 
-	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
-
-	"github.com/cometbft/cometbft/crypto/tmhash"
-	cometproto "github.com/cometbft/cometbft/proto/tendermint/types"
-	comettypes "github.com/cometbft/cometbft/types"
-	comettime "github.com/cometbft/cometbft/types/time"
-
 	transfertypes "github.com/cosmos/ibc-go/v8/modules/apps/transfer/types"
 	ibcclientutils "github.com/cosmos/ibc-go/v8/modules/core/02-client/client/utils"
 	clienttypes "github.com/cosmos/ibc-go/v8/modules/core/02-client/types"
@@ -31,9 +22,7 @@ import (
 	ibcexported "github.com/cosmos/ibc-go/v8/modules/core/exported"
 	tmclient "github.com/cosmos/ibc-go/v8/modules/light-clients/07-tendermint"
 	ibctesting "github.com/cosmos/ibc-go/v8/testing"
-	ibcmocks "github.com/cosmos/ibc-go/v8/testing/mock"
 
-	"github.com/strangelove-ventures/interchaintest/v8/chain/cosmos"
 	"github.com/strangelove-ventures/interchaintest/v8/chain/ethereum/foundry"
 	"github.com/strangelove-ventures/interchaintest/v8/ibc"
 	"github.com/strangelove-ventures/interchaintest/v8/testutil"
@@ -188,6 +177,7 @@ func (s *SP1ICS07TendermintTestSuite) TestUpdateClient() {
 	}))
 }
 
+// TestUpdateClientAndMembership tests the update client and membership functionality
 func (s *SP1ICS07TendermintTestSuite) TestUpdateClientAndMembership() {
 	ctx := context.Background()
 
@@ -244,6 +234,8 @@ func (s *SP1ICS07TendermintTestSuite) TestUpdateClientAndMembership() {
 	}))
 }
 
+// TestMisbehaviour tests the misbehaviour functionality
+// Partially based on https://github.com/cosmos/relayer/blob/f9aaf3dd0ebfe99fbe98d190a145861d7df93804/interchaintest/misbehaviour_test.go#L38
 func (s *SP1ICS07TendermintTestSuite) TestMisbehaviour() {
 	ctx := context.Background()
 
@@ -252,31 +244,56 @@ func (s *SP1ICS07TendermintTestSuite) TestMisbehaviour() {
 	eth, simd := s.ChainA, s.ChainB
 	_ = eth
 
-	s.Require().True(s.Run("Misbehave", func() {
-		// Based off of: https://github.com/cosmos/relayer/blob/f9aaf3dd0ebfe99fbe98d190a145861d7df93804/interchaintest/misbehaviour_test.go#L38
-		oldHeader, latestHeight, err := ibcclientutils.QueryTendermintHeader(simd.Validators[0].CliContext())
+	var height clienttypes.Height
+	var oldHeader tmclient.Header
+	s.Require().True(s.Run("Get old header", func() {
+		var latestHeight int64
+		var err error
+		oldHeader, latestHeight, err = ibcclientutils.QueryTendermintHeader(simd.Validators[0].CliContext())
 		s.Require().NoError(err)
 		s.Require().NotZero(latestHeight)
 
-		height := clienttypes.NewHeight(clienttypes.ParseChainID(simd.Config().ChainID), uint64(latestHeight))
+		height = clienttypes.NewHeight(clienttypes.ParseChainID(simd.Config().ChainID), uint64(latestHeight))
 
 		clientState, err := s.contract.GetClientState(nil)
 		s.Require().NoError(err)
 		trustedHeight := clienttypes.NewHeight(uint64(clientState.LatestHeight.RevisionNumber), uint64(clientState.LatestHeight.RevisionHeight))
 
 		oldHeader.TrustedHeight = trustedHeight
-		oldHeader.TrustedValidators = oldHeader.ValidatorSet // ?
-		s.Require().NoError(err)
+		oldHeader.TrustedValidators = oldHeader.ValidatorSet
+	}))
 
+	s.Require().True(s.Run("Invalid misbehaviour", func() {
+		// Create a new valid header
+		newHeader := s.CreateTMClientHeader(
+			ctx,
+			simd,
+			int64(height.RevisionHeight+1),
+			oldHeader.GetTime().Add(time.Minute),
+			oldHeader,
+		)
+
+		invalidMisbehaviour := tmclient.Misbehaviour{
+			Header1: &newHeader,
+			Header2: &oldHeader,
+		}
+
+		// The proof should fail because this is not misbehaviour (valid header for a new block)
+		_, err := operator.Misbehaviour(simd.GetCodec(), invalidMisbehaviour, false,
+			"--trust-level", testvalues.DefaultTrustLevel.String(),
+			"--trusting-period", strconv.Itoa(testvalues.DefaultTrustPeriod))
+		s.Require().ErrorContains(err, "Misbehaviour is not detected")
+	}))
+
+	s.Require().True(s.Run("Valid misbehaviour", func() {
 		// create a duplicate header (with a different hash)
-		newHeader := s.createTMClientHeader(
+		newHeader := s.CreateTMClientHeader(
 			ctx,
 			simd,
 			int64(height.RevisionHeight),
 			oldHeader.GetTime().Add(time.Minute),
 			oldHeader,
 		)
-		_ = newHeader
 
 		misbehaviour := tmclient.Misbehaviour{
 			Header1: &newHeader,
@@ -294,127 +311,8 @@ func (s *SP1ICS07TendermintTestSuite) TestMisbehaviour() {
 		// wait until transaction is included in a block
 		_ = s.GetTxReciept(ctx, eth.EthereumChain, tx.Hash())
 
-		clientState, err = s.contract.GetClientState(nil)
+		clientState, err := s.contract.GetClientState(nil)
 		s.Require().NoError(err)
 		s.Require().True(clientState.IsFrozen)
 	}))
-}
-
-func (s *SP1ICS07TendermintTestSuite) createTMClientHeader(
-	ctx context.Context,
-	chain *cosmos.CosmosChain,
-	blockHeight int64,
-	timestamp time.Time,
-	oldHeader tmclient.Header,
-) tmclient.Header {
-	var privVals []comettypes.PrivValidator
-	var validators []*comettypes.Validator
-	for _, chainVal := range chain.Validators {
-		keyBz, err := chainVal.ReadFile(ctx, "config/priv_validator_key.json")
-		s.Require().NoError(err)
-		var privValidatorKeyFile cosmos.PrivValidatorKeyFile
-		err = json.Unmarshal(keyBz, &privValidatorKeyFile)
-		s.Require().NoError(err)
-		decodedKeyBz, err := base64.StdEncoding.DecodeString(privValidatorKeyFile.PrivKey.Value)
-		s.Require().NoError(err)
-
-		privKey := &ed25519.PrivKey{
-			Key: decodedKeyBz,
-		}
-
-		privVal := ibcmocks.PV{PrivKey: privKey}
-		privVals = append(privVals, privVal)
-
-		pubKey, err := privVal.GetPubKey()
-		s.Require().NoError(err)
-
-		val := comettypes.NewValidator(pubKey, oldHeader.ValidatorSet.Proposer.VotingPower)
-		validators = append(validators, val)
-
-	}
-
-	valSet := comettypes.NewValidatorSet(validators)
-	vsetHash := valSet.Hash()
-
-	// Make sure all the signers are in the correct order as expected by the validator set
-	signers := make([]comettypes.PrivValidator, valSet.Size())
-	for i := range signers {
-		_, val := valSet.GetByIndex(int32(i))
-
-		for _, pv := range privVals {
-			pk, err := pv.GetPubKey()
-			s.Require().NoError(err)
-
-			if pk.Equals(val.PubKey) {
-				signers[i] = pv
-				break
-			}
-		}
-
-		if signers[i] == nil {
-			s.Require().FailNow("could not find signer for validator")
-		}
-	}
-
-	tmHeader := comettypes.Header{
-		Version:            oldHeader.Header.Version,
-		ChainID:            oldHeader.Header.ChainID,
-		Height:             blockHeight,
-		Time:               timestamp,
-		LastBlockID:        ibctesting.MakeBlockID(make([]byte, tmhash.Size), 10_000, make([]byte, tmhash.Size)),
-		LastCommitHash:     oldHeader.Header.LastCommitHash,
-		DataHash:           tmhash.Sum([]byte("data_hash")),
-		ValidatorsHash:     vsetHash,
-		NextValidatorsHash: vsetHash,
-		ConsensusHash:      tmhash.Sum([]byte("consensus_hash")),
-		AppHash:            tmhash.Sum([]byte("app_hash")),
-		LastResultsHash:    tmhash.Sum([]byte("last_results_hash")),
-		EvidenceHash:       tmhash.Sum([]byte("evidence_hash")),
-		ProposerAddress:    valSet.Proposer.Address,
-	}
-
-	hhash := tmHeader.Hash()
-	blockID := ibctesting.MakeBlockID(hhash, oldHeader.Commit.BlockID.PartSetHeader.Total, tmhash.Sum([]byte("part_set")))
-	voteSet := comettypes.NewVoteSet(oldHeader.Header.ChainID, blockHeight, 1, cometproto.PrecommitType, valSet)
-
-	voteProto := &comettypes.Vote{
-		ValidatorAddress: nil,
-		ValidatorIndex:   -1,
-		Height:           blockHeight,
-		Round:            1,
-		Timestamp:        comettime.Now(),
-		Type:             cometproto.PrecommitType,
-		BlockID:          blockID,
-	}
-
-	for i, sign := range signers {
-		pv, err := sign.GetPubKey()
-		s.Require().NoError(err)
-		addr := pv.Address()
-		vote := voteProto.Copy()
-		vote.ValidatorAddress = addr
-		vote.ValidatorIndex = int32(i)
-		_, err = comettypes.SignAndCheckVote(vote, sign, oldHeader.Header.ChainID, false)
-		s.Require().NoError(err)
-		added, err := voteSet.AddVote(vote)
-		s.Require().NoError(err)
-		s.Require().True(added)
-	}
-	extCommit := voteSet.MakeExtendedCommit(comettypes.DefaultABCIParams())
-	commit := extCommit.ToCommit()
-
-	signedHeader := &cometproto.SignedHeader{
-		Header: tmHeader.ToProto(),
-		Commit: commit.ToProto(),
-	}
-
-	valSetProto, err := valSet.ToProto()
-	s.Require().NoError(err)
-
-	return tmclient.Header{
-		SignedHeader:      signedHeader,
-		ValidatorSet:      valSetProto,
-		TrustedHeight:     oldHeader.TrustedHeight,
-		TrustedValidators: oldHeader.TrustedValidators,
-	}
 }

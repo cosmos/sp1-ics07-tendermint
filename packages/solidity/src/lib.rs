8317654692514f565f4179a66e3923d8d032c944
srdtrk <srdtrk@hotmail.com>
2024-10-02 14:55:15 +0300

    refactor: refactored height conversion

--- packages/solidity/src/lib.rs
+++ packages/solidity/src/lib.rs
@@ -4,6 +4,7 @@
 
 use alloy_sol_types::SolValue;
 use ibc_client_tendermint_types::ConsensusState as ICS07TendermintConsensusState;
+use ibc_core_client_types::Height;
 use ibc_core_commitment_types::commitment::CommitmentRoot;
 use tendermint::{hash::Algorithm, Time};
 use tendermint_light_client_verifier::types::{Hash, TrustThreshold as TendermintTrustThreshold};
@@ -129,5 +130,16 @@ impl From<sp1_ics07_tendermint::SP1MembershipAndUpdateClientProof>
             proofType: 1,
             proof: proof.abi_encode().into(),
         }
+    }
+}
+
+impl TryFrom<Height> for sp1_ics07_tendermint::Height {
+    type Error = <u64 as TryInto<u32>>::Error;
+
+    fn try_from(height: Height) -> Result<Self, Self::Error> {
+        Ok(Self {
+            revisionNumber: height.revision_number().try_into()?,
+            revisionHeight: height.revision_height().try_into()?,
+        })
     }
 }

cosmos/sp1-ics07-tendermint

commit 622dd399b110525e4ef831cf91ca61eab71a3e06

imp: use `Vec::with_capacity` avoid realloc mem (#123)
* use Vec::with_capacity avoid realloc mem

* imp: imp

---------

Co-authored-by: srdtrk <srdtrk@hotmail.com>

packages/utils/src/lib.rs
@@ -12,14 +12,12 @@ use tendermint::merkle::proof::ProofOps;
 pub fn convert_tm_to_ics_merkle_proof(
     tm_proof: &ProofOps,
 ) -> Result<MerkleProof, prost::DecodeError> {
-    let mut proofs = Vec::new();
+    let mut proofs = Vec::with_capacity(tm_proof.ops.len());
 
-    for op in &tm_proof.ops {
-        let mut parsed = CommitmentProof { proof: None };
-
+    for (i, op) in tm_proof.ops.iter().enumerate() {
+        let mut parsed = CommitmentProof::default();
         prost::Message::merge(&mut parsed, op.data.as_slice())?;
-
-        proofs.push(parsed);
+        proofs[i] = parsed;
     }
 
     Ok(MerkleProof { proofs })

commit 063ecac6342c8c83d56fd8168836daac24e9d2a1

fix: bug introduced in #123 (#125)

packages/utils/src/lib.rs
@@ -14,10 +14,12 @@ pub fn convert_tm_to_ics_merkle_proof(
 ) -> Result<MerkleProof, prost::DecodeError> {
     let mut proofs = Vec::with_capacity(tm_proof.ops.len());
 
-    for (i, op) in tm_proof.ops.iter().enumerate() {
-        let mut parsed = CommitmentProof::default();
+    for op in &tm_proof.ops {
+        let mut parsed = CommitmentProof { proof: None };
+
         prost::Message::merge(&mut parsed, op.data.as_slice())?;
-        proofs[i] = parsed;
+
+        proofs.push(parsed);
     }
 
     Ok(MerkleProof { proofs })